f937f567ffb6a0a8a84bfb82b4ab0a300a560b09
Vaucher Malina <mvaucher@INTRANET.EPFL.CH@SVUbuUniv11.intranet.epfl.ch>
2019-11-01 10:09:42 +0100

    Rendu final

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mvaucher/myfiles/Programmation/BA3/NeuronNet-master
-# Build directory: /home/mvaucher/myfiles/Programmation/BA3/NeuronNet-master/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CMake_Neuronal_Network "testNeuronNet")
